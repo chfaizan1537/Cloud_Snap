cb387a1a533727f3a4c661f560e9c1fe74702658
chfaizan1537 <chf69737@gmail.com>
2026-03-19 11:30:05 +0500

    some changes done

--- lib/main.dart
+++ lib/main.dart
@@ -2,13 +2,18 @@ import 'package:cloud_snap4/routes/routes_initilization.dart';
 import 'package:cloud_snap4/routes/routes_names.dart';
 import 'package:cloud_snap4/views/splash_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:get/get.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 Future<void> main() async{
+  await dotenv.load(fileName: ".env");
+
+  final supabaseUrl = dotenv.env['SUPABASE_URL']!;
+  final supabaseKey = dotenv.env['SUPABASE_ANON_KEY']!;
   await Supabase.initialize(
-    url: 'https://klblgqijzpetxxcbdtft.supabase.co',
-    anonKey: 'sb_publishable_irsx5vfIdYB80mFeRcAXYg_onq3QaOV',
+    url: supabaseUrl,
+    anonKey: supabaseKey,
   );
   runApp(const mainScreen());
 }
